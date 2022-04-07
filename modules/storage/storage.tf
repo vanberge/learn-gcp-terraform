5397f53a136a971b22e969e74de5370940ca5b6f
Eric VanBergen <vanberge@gmail.com>
2022-04-07 13:56:27 -0400

    add uniform bucket level access

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "gcs_bucket" {
-  name          = var.project_id
-  location      = "US"
-  storage_class = "Standard"
+  name                        = var.project_id
+  location                    = "US"
+  storage_class               = "Standard"
+  uniform_bucket_level_access = true
 }
