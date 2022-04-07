7e5378ac96c3c9be5185a6c9b6231f70f603f773
Eric VanBergen <vanberge@gmail.com>
2022-04-07 13:39:27 -0400

    terraform fmt changes

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ module "gke" {
   k8s_network        = module.network.k8s_network #Enable the vpc to be passed to the k8s module
   k8s_network_subnet = module.network.k8s_network_subnet
 }
-module "storage"{
-  source  = "./modules/storage"
+module "storage" {
+  source = "./modules/storage"
 }
--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "gcs_bucket" {
   name          = var.project_id
   location      = "US"
-  storage_class = "Standard"  
+  storage_class = "Standard"
 }
